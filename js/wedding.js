ab82c00ddfb026c2e5cb6ad546f37c4d579bd24e
jamesrhaley <james.r.haley@gmail.com>
2015-03-01 18:29:25 -0600

    cleaned up style and add pictures

--- js/wedding.js
+++ js/wedding.js
@@ -1,6 +1,7 @@
-var SIDEMENU = function ( wind ) {
+var SIDEMENU = function () {
     "use strict";
-    var window_Doc = wind.document,
+    var wind = window,
+        window_Doc = wind.document,
         navScroll,
         diveInDoc = function (wind) {
             return wind.trim ? wind.trim() : wind.replace(/^\s+|\s+$/g, "");
@@ -9,23 +10,23 @@ var SIDEMENU = function ( wind ) {
             return -1 !== (" " + wind.className + " ").indexOf(" " + window_Doc + " ");
         },
         getClassSize = function (wind, window_Doc) {
-            getClass(wind, window_Doc) || (wind.className = "" === wind.className ? window_Doc : wind.className + " " + window_Doc);
+            return getClass(wind, window_Doc) || (wind.className = "" === wind.className ? window_Doc : wind.className + " " + window_Doc);
         },
         checkViewPort = function (wind, window_Doc) {
-            wind.className = diveInDoc((" " + wind.className + " ").replace(" " + window_Doc + " ", " "));
+            return wind.className = diveInDoc((" " + wind.className + " ").replace(" " + window_Doc + " ", " "));
         },
         checkValue = function (wind, window_Doc) {
             if (wind) {
                 do {
                     if (wind.id === window_Doc) {
-                        return !0;
+                        return 1;
                     }
                     if (9 === wind.nodeType) {
                         break;
                     }
                 }
                 while (wind = wind.parentNode) {
-                    return !1;
+                    return 0;
                 }
             }
         },
@@ -39,32 +40,32 @@ var SIDEMENU = function ( wind ) {
                     msTransition: "MSTransitionEnd",
                     transition: "transitionend"
                 };
-            return vederObject.hasOwnProperty(modernGroup) ? vederObject[modernGroup] : !1;
+            return vederObject.hasOwnProperty(modernGroup) ? vederObject[modernGroup] : 0;
         };
         
 
     navScroll = (function () {
         var navObject = {},
-            isOpen = !1,
+            isOpen = 0,
             innerWrap = window_Doc.getElementById("inner-wrap"),
-            m = !1,
+            m = 0,
             addClassName = "js-nav",
             recall = function ( trans_event ) {
-                trans_event && trans_event.target === innerWrap && window_Doc.removeEventListener(venderNames, recall, !1);
-                m = !1;
+                trans_event && trans_event.target === innerWrap && window_Doc.removeEventListener(venderNames, recall, 0);
+                m = 0;
             };
 
         navObject.closeNav = function () {
                 if (m) {
                     var c = venderNames && modernGroup ? parseFloat(wind.getComputedStyle(innerWrap, "")[modernGroup + "Duration"]) : 0;
-                    c > 0 ? window_Doc.addEventListener(venderNames, recall, !1) : recall(null);
+                    c > 0 ? window_Doc.addEventListener(venderNames, recall, 0) : recall(null);
                 }
                 checkViewPort(element, addClassName);
-                m = !1;
+                m = 0;
             };
 
         navObject.openNav = function () {
-                m || ( getClassSize(element, addClassName), m = !0 );
+                m || ( getClassSize(element, addClassName), m = 1 );
             };
 
         navObject.toggleNav = function ( mouse_toggle ) {
@@ -73,13 +74,13 @@ var SIDEMENU = function ( wind ) {
             };
         navObject.init = function () {
             if (!isOpen) {
-                isOpen = !0;
+                isOpen = 1;
 
-                window_Doc.getElementById( "nav-open-btn" ).addEventListener( "click", navObject.toggleNav, !1 );
-                window_Doc.getElementById( "nav-close-btn" ).addEventListener( "click", navObject.toggleNav, !1 );
+                window_Doc.getElementById( "nav-open-btn" ).addEventListener( "click", navObject.toggleNav, 0 );
+                window_Doc.getElementById( "nav-close-btn" ).addEventListener( "click", navObject.toggleNav, 0 );
                 window_Doc.addEventListener("click", function ( mouse_toggle ) {
                     m && !checkValue( mouse_toggle.target, "nav" ) && ( mouse_toggle.preventDefault(), navObject.closeNav() );
-                }, !0);
+                }, 1);
                 getClassSize( element, "js-ready" );
             }
         }
@@ -87,10 +88,10 @@ var SIDEMENU = function ( wind ) {
         return navObject;
     })()
 
-    return wind.addEventListener && wind.addEventListener( "DOMContentLoaded", navScroll.init, !1 );
+    return wind.addEventListener && wind.addEventListener( "DOMContentLoaded", navScroll.init, 0 );
 }
 
-SIDEMENU( window );
+SIDEMENU();
 
 
 
